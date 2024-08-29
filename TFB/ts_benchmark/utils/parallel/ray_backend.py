84b7a39e10f2b249f0912fe5e239aefcf35e4195
lzy <1016984801@qq.com>
2024-08-29 16:08:17 +0800

    translate

--- TFB/ts_benchmark/utils/parallel/ray_backend.py
+++ TFB/ts_benchmark/utils/parallel/ray_backend.py
@@ -22,7 +22,7 @@ logger = logging.getLogger(__name__)
 
 def is_actor() -> bool:
     """
-    判断当前是否正在 actor 中运行
+    Determine whether it is currently running in the actor.
     """
     return ray.get_runtime_context().worker.mode == ray.WORKER_MODE
 
@@ -109,9 +109,9 @@ class RayTask:
 
 class RayActorPool:
     """
-    ray actor 资源池
+    Ray actor resource pool
 
-    和 ray 的内置 ActorPool 不同，本实现试图支持为每个任务限时
+    Unlike ray's built-in ActorPool, this implementation attempts to support time limiting for each task.
     """
 
     def __init__(
